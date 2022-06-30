cb1326a34d68bb7494b651fd3601ccfc51832952
atencion <maxmaxmax@gmail.com>
2022-06-30 19:58:12 -0300

    white triangles

--- src/App.js
+++ src/App.js
@@ -50,9 +50,10 @@ export default function App() {
       <OrbitControls enableZoom={false} enablePan={false} enableRotate={false} />
       <Suspense fallback={null}>
         <Rig>
-          <Triangle color="#ff2060" scale={0.009} rotation={[0, 0, Math.PI / 3]} />
-          <Triangle color="cyan" scale={0.009} position={[2, 0, -2]} rotation={[0, 0, Math.PI / 3]} />
-          <Triangle color="orange" scale={0.009} position={[-2, 0, -2]} rotation={[0, 0, Math.PI / 3]} />
+          {/*           <Triangle color="#ff2060" scale={0.009} rotation={[0, 0, Math.PI / 3]} /> */}
+          <Triangle color="white" scale={0.009} rotation={[0, 0, Math.PI / 3]} />
+          <Triangle color="white" scale={0.009} position={[2, 0, -2]} rotation={[0, 0, Math.PI / 3]} />
+          <Triangle color="white" scale={0.009} position={[-2, 0, -2]} rotation={[0, 0, Math.PI / 3]} />
           <Triangle color="white" scale={0.009} position={[0, 2, -10]} rotation={[0, 0, Math.PI / 3]} />
           <Ground mirror={1} blur={[500, 100]} mixBlur={12} mixStrength={1.5} rotation={[-Math.PI / 2, 0, Math.PI / 2]} position-y={-0.8} />
         </Rig>
